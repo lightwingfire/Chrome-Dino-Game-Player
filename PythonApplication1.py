95531278266aa6816e0302e8e06ab8320272a360
lightwingfire <41173683+lightwingfire@users.noreply.github.com>
2019-11-09 18:46:36 -0500

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -8,24 +8,31 @@ def main():
     screen = ImageGrab.grab()
     screenWidth = screen.width
     screenHeight = screen.height
+    x1 = (520/1920) * screenWidth
+    y1 = (570/1080) * screenHeight
+    x2 = (600/1920) * screenWidth
+    y2 = (670/1080) * screenHeight
     detecting = False
     while True:
         if keyboard.is_pressed('m'):
+            print("start")
             detecting = True 
         elif keyboard.is_pressed('n'):
+            print("stop")
             detecting = False
         if detecting == True: 
-            detectObstacle(520,570,600,670)
-        #print(backgroundColor(520, 800))
+            detectObstacle(x1,y1,x2,y2)
+        #print(backgroundColor(520, 800)) 520,570,600,670
 def detectObstacle(x1,y1,x2,y2):
    p = ImageGrab.grab(bbox = (x1,y1,x2,y2))
    j = p.getcolors()
    (num, tup) = j[0]
-   print(num)
+   #print(num)
    if (num == ((x2-x1)*(y2-y1))):
        keyboard.release('space')
    else:
        keyboard.press('space')
+       print("jump!")
 
 def backgroundColor(x,y):
     pixel = ImageGrab.grab(bbox = (x,y,x+1,y+1))
